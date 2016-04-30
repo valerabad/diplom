1ad9c9f446209be3ce9715c5ba1cd1156b4603a5
valerabad <valerabaditsa@gmail.com>
2016-04-30 22:50:30 +0300

    1

--- myProject/myProject/Program.cs
+++ myProject/myProject/Program.cs
@@ -24,7 +24,7 @@ namespace myProject
             Something(Singletone.Instance);                                 
             Console.ReadKey();
         }
-        // new cjanging in master
+        
         static public void Something(Singletone singl)
         {
  
